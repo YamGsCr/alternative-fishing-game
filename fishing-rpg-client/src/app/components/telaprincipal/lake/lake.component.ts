029d74fe478dd30558bacd58a304a5b765574fe8
martins-ggm <guilherme.ggm@outlook.com>
2025-05-14 20:40:20 -0300

    mudança no layout da tela principa

--- fishing-rpg-client/src/app/components/telaprincipal/lake/lake.component.ts
+++ fishing-rpg-client/src/app/components/telaprincipal/lake/lake.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from '../../../features/layout/top-bar/top-bar.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-lake',
-  imports: [RouterOutlet,TopBarComponent],
+  imports: [RouterOutlet,TopBarComponent,NgOptimizedImage],
   templateUrl: './lake.component.html',
   styleUrl: './lake.component.scss'
 })
