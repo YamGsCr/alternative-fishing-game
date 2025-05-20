d4110fce0a010316f9661737d571e52b456b06ed
martins-ggm <guilherme.ggm@outlook.com>
2025-05-19 23:58:02 -0300

    Ajustes no layout, overflow-scroll fixed, implementação das abas na seção central

--- fishing-rpg-client/src/app/features/layout/mapas/mapa.component.ts
+++ fishing-rpg-client/src/app/features/layout/mapas/mapa.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
+
 @Component({
   selector: 'app-mapa',
   standalone: true,
   imports: [CommonModule],
-  template: `<div class="mock">[ Mapa do Mundo Aqui ]</div>`, //Ta inline, quando tu for fazer o css e html, faz um templateUrl e importa
+  templateUrl:'./mapa.component.html', //Ta inline, quando tu for fazer o css e html, faz um templateUrl e importa
   styles: [`.mock { padding: 20px; font-size: 20px; text-align: center; }`]
 })
 export class MapaComponent {}
--- fishing-rpg-client/src/app/features/layout/mapas/mapas.module.ts
+++ fishing-rpg-client/src/app/features/layout/mapas/mapas.module.ts
@@ -3,7 +3,10 @@ import { MapaComponent } from "./mapa.component";
 import { CommonModule } from "@angular/common";
 import { MapaRoutingModule } from "./mapa-routing.module";
 
+
+
 @NgModule({
+    
     declarations: [MapaComponent],
     imports: [CommonModule, MapaRoutingModule]
 })
